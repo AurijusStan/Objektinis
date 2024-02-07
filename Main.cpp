7f9abb593be41a9bdf85147d2458ab0df781532d
Aurijus Stanislovaitis <aurijus.stan@outlook.com>
2024-02-07 17:32:48 +0200

    .

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
+#include <iomanip>
+#include <numeric>
 
 using namespace std;
+using std::setw;
+using std::left;
 
 struct duom{
     string vard;
     int n;
-    double ndrez[ndkiek];
-    double egzrez;
+    int ndrez[20];
+    int egzrez;
 };
 
-int main(){
-    freopen("duota.txt", "r", stdin);
+void input(duom mokinys[], int j){
+    // freopen("duota.txt", "r", stdin);
 
+    cout<<"Įrašykite vardą ir pavardę";
+    cin>>mokinys[j].vard;
+    cout<<"Įrašykite namų darbų skaičių";
+    cin>>mokinys[j].n;
+    cout<<"Įrašykite nd rezultatus po kiekvieno spaudžiant enter";
+    for(int i=0; i<mokinys[j].n; i++){
+        cin>>mokinys[j].ndrez[i];
+    }
+    cout<<"Įrašykite egzamino rezultatą";
+    cin>>mokinys[j].egzrez;
+
+    return;
+}
+
+void calc(duom mokinys[], int j){
+    if(mokinys[j].n!=0){
+        std::accumulate
+    }
+
+}
+
+int main(){    
     int x;
 
     cout<<"Įrašykite mokinių skaičių";
@@ -19,14 +45,16 @@ int main(){
 
     duom mokinys[x+1];
 
-    cout<<"Įrašykite vardą ir pavardę";
-    cin>>mokinys.vard;
-    cout<<"Įrašykite namų darbų skaičių";
-    cin>>mokinys.n;
-    cout<<"Įrašykite nd rezultatus po kiekvieno spaudžiant enter";
-    for(int i=0; i<n; i++){
-        cin>>mokinys.ndrez[i];
+    for(int i=0; i<x; i++){
+        input(mokinys, i);
     }
-    cout<<"Įrašykite egzamino rezultatą";
-    cin>>mokinys.egzrez;
+
+    cout<<setw(7)<<left<<"Vardas"<<"Pavardė"<<"Galutinis (Vid.)"<<endl;
+    cout<<"------------------------------------------------";
+
+    for(int i=0; i<x; i++){
+        calc(mokinys, i);
+    }
+
+    return 0;
 }
