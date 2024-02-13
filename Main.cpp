9a3c7812268677590da4368f7b5c7a707116337e
AurijusStan <aurijus.stan@outlook.com>
2024-02-13 12:38:58 +0200

    Veikianti versija

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,7 @@ using std::left;
 
 struct duom{
     string vard;
+    string pav;
     int n;
     int ndrez[20];
     int egzrez;
@@ -16,31 +17,41 @@ struct duom{
 void input(duom mokinys[], int j){
     // freopen("duota.txt", "r", stdin);
 
-    cout<<"Įrašykite vardą ir pavardę";
+    cout<<"Irasykite varda"<<endl;
     cin>>mokinys[j].vard;
-    cout<<"Įrašykite namų darbų skaičių";
+    cout<<"Irasykite pavarde"<<endl;
+    cin>>mokinys[j].pav;
+    cout<<"Irasykite namu darbu skaiciu"<<endl;
     cin>>mokinys[j].n;
-    cout<<"Įrašykite nd rezultatus po kiekvieno spaudžiant enter";
-    for(int i=0; i<mokinys[j].n; i++){
-        cin>>mokinys[j].ndrez[i];
+    if(mokinys[j].n!=0){
+        cout<<"Irasykite nd rezultatus po kiekvieno spaudziant enter"<<endl;
+        for(int i=0; i<mokinys[j].n; i++){
+            cin>>mokinys[j].ndrez[i];
+        }
     }
-    cout<<"Įrašykite egzamino rezultatą";
+    cout<<"Irasykite egzamino rezultata"<<endl;
     cin>>mokinys[j].egzrez;
 
     return;
 }
 
 void calc(duom mokinys[], int j){
+    double sum=0;
+    double egz=0;
     if(mokinys[j].n!=0){
-        std::accumulate
+        for(int i=0; i<mokinys[j].n; i++){
+            sum+=mokinys[j].ndrez[i];
+        }
+        sum=sum/mokinys[j].n;
     }
-
+    egz=mokinys[j].egzrez;
+    cout<<setw(17)<<left<<mokinys[j].vard<<setw(17)<<left<<mokinys[j].pav<<setw(17)<<left<<(sum*0.4+egz*0.6)<<endl;
 }
 
 int main(){    
     int x;
 
-    cout<<"Įrašykite mokinių skaičių";
+    cout<<"Irasykite mokiniu skaiciu"<<endl;
     cin>>x;
 
     duom mokinys[x+1];
@@ -49,8 +60,8 @@ int main(){
         input(mokinys, i);
     }
 
-    cout<<setw(7)<<left<<"Vardas"<<"Pavardė"<<"Galutinis (Vid.)"<<endl;
-    cout<<"------------------------------------------------";
+    cout<<setw(17)<<left<<"Vardas"<<setw(17)<<left<<"Pavarde"<<setw(17)<<left<<"Galutinis (Vid.)"<<endl;
+    cout<<"-------------------------------------------------------"<<endl;
 
     for(int i=0; i<x; i++){
         calc(mokinys, i);
