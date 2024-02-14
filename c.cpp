2dfe537de1897a6d87c1a8a14746669e5acb32c2
AurijusStan <aurijus.stan@outlook.com>
2024-02-14 23:49:05 +0200

    C masyvai neveikia, taisysiu

--- c.cpp
+++ c.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iomanip>
 #include <numeric>
-#include <vector>
 #include <random>
 
 using namespace std;
@@ -11,22 +10,23 @@ using std::left;
 struct duom{
     string vard;
     string pav;
-    int n=-1;
-    vector<int> ndrez;
+    int uzimta;
+    int skirta;
+    int* ndrez;
     int egzrez;
 };
 
 vector<duom> mok;
 
-void sort(int k){
-    for(int i=0; i<mok[k].n+1; i++){
-        for(int j=i+1; j<mok[k].n+1; j++){
+void sort(duom* mok, int k){
+    for(int i=0; i<mok[k].uzimta+1; i++){
+        for(int j=i+1; j<mok[k].uzimta+1; j++){
             if(mok[k].ndrez[i]>mok[k].ndrez[j]) swap(mok[k].ndrez[i], mok[k].ndrez[j]);
         }
     }
 }
 
-void input(int &kiek){
+void input(duom* mok, int &uzimta, int &skirta){
 
     srand(time(nullptr));
 
@@ -55,71 +55,89 @@ void input(int &kiek){
 
     if(x==4) exit(0);
 
-    mok.push_back(duom());
-    kiek++;
-
     if(x==3){
         for(int i=0; i<4+rand()%6; i++){
-            if(i==0) mok[kiek].vard+=(char) (65+rand()%25);
-            else mok[kiek].vard+=(char) (97+rand()%25);
+            if(i==0) mok[uzimta].vard+=(char) (65+rand()%25);
+            else mok[uzimta].vard+=(char) (97+rand()%25);
         }
         for(int i=0; i<4+rand()%8; i++){
-            if(i==0) mok[kiek].pav+=(char) (65+rand()%25);
-            else mok[kiek].pav+=(char) (97+rand()%25);
+            if(i==0) mok[uzimta].pav+=(char) (65+rand()%25);
+            else mok[uzimta].pav+=(char) (97+rand()%25);
         }
     }
     else{
         cout<<"Irasykite varda"<<endl;
-        cin>>mok[kiek].vard;
+        cin>>mok[uzimta].vard;
         cout<<"Irasykite pavarde"<<endl;
-        cin>>mok[kiek].pav;
+        cin>>mok[uzimta].pav;
     }
     
+    mok[uzimta].uzimta=0;
+    mok[uzimta].skirta=1;
+    mok[uzimta].ndrez = new int[mok[uzimta].skirta];
+
     if(x==2||x==3){
         for(int i=0; i<1+rand()%10; i++){
-            mok[kiek].ndrez.push_back(rand()%11);
-            mok[kiek].n++;
+            if(mok[uzimta].uzimta==mok[uzimta].skirta){
+                mok[uzimta].skirta+=5;
+                int* temp = new int[mok[uzimta].skirta];
+                for(int i=0; i<mok[uzimta].uzimta; i++){
+                    temp[i]=mok[uzimta].ndrez[i];
+                }
+                delete[] mok[uzimta].ndrez;
+                mok[uzimta].ndrez=temp;
+            }
+            mok[uzimta].ndrez[mok[uzimta].uzimta]=rand()%11;
+            mok[uzimta].uzimta++;
         }
     }
     else{
         cout<<"Irasykite nd rezultatus po kiekvieno spaudziant enter, jei baigete parasykite skaiciu netelpanti i desimtbales sistemos intervala"<<endl;
         while(true){
-            mok[kiek].ndrez.push_back(0);
-            mok[kiek].n++;
+            if(mok[uzimta].uzimta==mok[uzimta].skirta){
+                mok[uzimta].skirta+=5;
+                int* temp = new int[mok[uzimta].skirta];
+                for(int i=0; i<mok[uzimta].uzimta; i++){
+                    temp[i]=mok[uzimta].ndrez[i];
+                }
+                delete[] mok[uzimta].ndrez;
+                mok[uzimta].ndrez=temp;
+            }
             while(true){
-                if(cin>>mok[kiek].ndrez[mok[kiek].n]) break;
+                if(cin>>mok[uzimta].ndrez[mok[uzimta].uzimta]) break;
                 else{
                     cin.clear();
                     cin.ignore();
                     cout<<"Klaidingai ivesti duomenys, iveskite sveika skaiciu"<<endl;
                 }
             }
-            if(mok[kiek].ndrez[mok[kiek].n]<0||mok[kiek].ndrez[mok[kiek].n]>10){
-                mok[kiek].ndrez.pop_back();
-                mok[kiek].n--;
+            
+            if(mok[uzimta].ndrez[mok[uzimta].uzimta]<0||mok[uzimta].ndrez[mok[uzimta].uzimta]>10){
                 break;
             }
+            mok[uzimta].uzimta++;
         }
     }
-    sort(kiek);
+
+    sort(mok, uzimta);
 
     if(x==2||x==3){
-        mok[kiek].egzrez=rand()%11;
+        mok[uzimta].egzrez=rand()%11;
     }
     else{
         while(true){
             cout<<"Irasykite egzamino rezultata"<<endl;
-            if(cin>>mok[kiek].egzrez) break;
+            if(cin>>mok[uzimta].egzrez) break;
             else{
                 cin.clear();
                 cin.ignore();
                 cout<<"Klaidingai ivesti duomenys"<<endl;
             }
         }
-        while(mok[kiek].egzrez<0||mok[kiek].egzrez>10){
+        while(mok[uzimta].egzrez<0||mok[uzimta].egzrez>10){
             cout<<"Ivertinimas turi buti desimtbaleje sistemoje, pabandykite dar karta"<<endl;
             while(true){
-                if(cin>>mok[kiek].egzrez) break;
+                if(cin>>mok[uzimta].egzrez) break;
                 else{
                     cin.clear();
                     cin.ignore();
@@ -140,27 +158,37 @@ void input(int &kiek){
             cout<<"Klaidingai ivesti duomenys"<<endl;
         }
     }
-    if(a==1) input(kiek);
-
+    if(a==1){
+        uzimta++;
+        if(uzimta==skirta){
+            skirta+=5;
+            duom* temp = new duom[skirta];
+            for(int i=0; i<uzimta; i++){
+                temp[i]=mok[i];
+            }
+            delete[] mok;
+            mok=temp;
+        }
+        input(mok, uzimta, skirta);
+    }
     return;
 }
 
-void calc(int j){
-    mok[j].n++;
+void calc(duom* mok, int j){
     double sum=0;
     double egz=0;
     double med=0;
-    if(mok[j].n!=0){
-        for(int i=0; i<(mok[j].n); i++){
+    if(mok[j].uzimta!=0){
+        for(int i=0; i<(mok[j].uzimta); i++){
             sum+=mok[j].ndrez[i];
         }
-        sum=sum/(mok[j].n);
-        if((mok[j].n)%2==0){
-            med=mok[j].ndrez[(mok[j].n/2)-1]+mok[j].ndrez[(mok[j].n/2)];
+        sum=sum/(mok[j].uzimta);
+        if((mok[j].uzimta)%2==0){
+            med=mok[j].ndrez[mok[j].uzimta/2]+mok[j].ndrez[(mok[j].uzimta+1)/2];
             med/=2;
         }
         else{
-            med=mok[j].ndrez[((mok[j].n+1)/2)-1];
+            med=mok[j].ndrez[((mok[j].uzimta+1)/2)];
         }
     }
     egz=mok[j].egzrez;
@@ -168,15 +196,17 @@ void calc(int j){
 }
 
 int main(){    
-    int kiek=-1;
+    int uzimta=0, skirta=1;
 
-    input(kiek);
+    duom* mok = new duom[skirta];
+
+    input(mok, uzimta, skirta);
 
     cout<<setw(19)<<left<<"Vardas"<<setw(19)<<left<<"Pavarde"<<setw(19)<<left<<"Galutinis (Vid.)"<<setw(19)<<left<<"Galutinis (Med.)"<<endl;
     cout<<"-------------------------------------------------------------------------"<<endl;
 
-    for(int i=0; i<kiek+1; i++){
-        calc(i);
+    for(int i=1; i<=uzimta+1; i++){
+        calc(mok, i);
     }
     
     return 0;
